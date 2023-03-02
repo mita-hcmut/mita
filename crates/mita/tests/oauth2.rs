ca68a09233f85333628a977f34de9b544a910d5f
Duy Khang <khangemail22@gmail.com>
2023-03-02 20:52:29 +0700

    use server in test

--- crates/mita/tests/oauth2.rs
+++ crates/mita/tests/oauth2.rs
@@ -1,14 +1,24 @@
+use mita::{config::Config, entrypoint::Server};
+
 mod helper;
 
 #[tokio::test]
-pub async fn oauth2() {
+pub async fn oauth2() -> eyre::Result<()> {
+    let config = Config::test()?;
+    let server = Server::build(&config).await?;
+    let addr = server.addr();
+
+    tokio::spawn(server);
+
     let res = helper::oauth2::get_code("khang", "").await;
     let client = reqwest::Client::new();
     let res = client
-        .put("http://localhost:3000/token")
+        .put(format!("http://{addr}/token"))
         .bearer_auth(&res.id_token)
         .send()
-        .await
-        .unwrap();
+        .await?;
+
     dbg!(res);
+
+    Ok(())
 }
