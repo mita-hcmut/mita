e065d1fc1c419d02327097487228bca41c8b7a2f
Duy Khang <khangemail22@gmail.com>
2023-03-05 14:52:26 +0700

    extract telemetry layer

--- crates/mita/src/routes/router.rs
+++ crates/mita/src/routes/router.rs
@@ -1,43 +1,19 @@
 use axum::{
-    body::Body,
-    http::Request,
     routing::{get, put},
     Router,
 };
-use tower::ServiceBuilder;
-use tower_http::{request_id::MakeRequestUuid, trace::TraceLayer, ServiceBuilderExt};
 
 use super::{info::get::get_info, root, token::put::register_token};
-use crate::{app_state::AppState, middlewares::vault::authenticate};
+use crate::{
+    app_state::AppState, middlewares::vault::authenticate, telemetry::router_telemetry_layer,
+};
 
 pub fn app_router(state: AppState) -> Router<()> {
     Router::new()
         .route("/", get(root))
         .merge(protected_router(state.clone()))
         .with_state(state)
-        .layer(
-            ServiceBuilder::new()
-                .set_x_request_id(MakeRequestUuid)
-                .layer(
-                    TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
-                        let request_id = request
-                            .headers()
-                            .get("x-request-id")
-                            .expect("x-request-id header not set")
-                            .to_str()
-                            .expect("x-request-id header not valid ascii");
-
-                        tracing::info_span!(
-                            "request",
-                            id = %request_id,
-                            method = %request.method(),
-                            uri = %request.uri(),
-                        )
-                    }),
-                )
-                .propagate_x_request_id()
-                .into_inner(),
-        )
+        .layer(router_telemetry_layer())
 }
 
 fn protected_router(state: AppState) -> Router<AppState> {
--- crates/mita/src/telemetry.rs
+++ crates/mita/src/telemetry.rs
@@ -1,3 +1,15 @@
+use axum::{body::Body, http::Request};
+use tower::{
+    layer::util::{Identity, Stack},
+    ServiceBuilder,
+};
+use tower_http::{
+    classify::{ServerErrorsAsFailures, SharedClassifier},
+    request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
+    trace::TraceLayer,
+    ServiceBuilderExt,
+};
+use tracing::Span;
 use tracing_chrome::{ChromeLayerBuilder, FlushGuard};
 use tracing_subscriber::{
     filter::{EnvFilter, LevelFilter},
@@ -20,4 +32,37 @@ pub fn setup() -> FlushGuard {
         .init();
 
     _guard
+}
+
+// extract the type so that clippy wont yell at me
+type RouterTelemetryLayer<MakeSpan> = Stack<
+    PropagateRequestIdLayer,
+    Stack<
+        TraceLayer<SharedClassifier<ServerErrorsAsFailures>, MakeSpan>,
+        Stack<SetRequestIdLayer<MakeRequestUuid>, Identity>,
+    >,
+>;
+
+pub fn router_telemetry_layer() -> RouterTelemetryLayer<impl Fn(&Request<Body>) -> Span + Clone> {
+    ServiceBuilder::new()
+        .set_x_request_id(MakeRequestUuid)
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
+                let request_id = request
+                    .headers()
+                    .get("x-request-id")
+                    .expect("x-request-id header not set")
+                    .to_str()
+                    .expect("x-request-id header not valid ascii");
+
+                tracing::info_span!(
+                    "request",
+                    id = %request_id,
+                    method = %request.method(),
+                    uri = %request.uri(),
+                )
+            }),
+        )
+        .propagate_x_request_id()
+        .into_inner()
 }
